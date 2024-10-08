10f5dc8a4b330863935dc16fd7d7186a87576357
Rajala1404 <115874847+Rajala1404@users.noreply.github.com>
2024-10-08 20:07:51 +0200

    Started implementation of entries for lessons

--- src/modules/lessons.rs
+++ src/modules/lessons.rs
@@ -1,11 +1,11 @@
 use std::time::SystemTime;
 use reqwest::Client;
-use scraper::{Html, Selector};
+use scraper::{ElementRef, Html, Selector};
 use crate::utils::constants::URL;
 
 #[derive(Debug)]
 pub struct Lessons {
-    lessons: Vec<Lesson>,
+    pub lessons: Vec<Lesson>,
 }
 
 #[derive(Debug)]
@@ -14,6 +14,8 @@ pub struct Lesson {
     pub url: String,
     pub name: String,
     pub teacher: String,
+    pub entry_latest: Option<LessonEntry>,
+    pub entries: Option<Vec<LessonEntry>>
 }
 
 #[derive(Debug)]
@@ -23,7 +25,8 @@ pub struct LessonEntry {
     pub title: String,
     pub details: Option<String>,
     pub homework: Option<Homework>,
-    pub attachment: Option<Vec<Attachment>>,
+    pub attachments: Option<Vec<Attachment>>,
+    pub attachment_number: i32,
     pub uploads: Option<LessonUpload>
 }
 
@@ -44,7 +47,6 @@ pub struct Homework {
 pub struct LessonUpload {
     pub url: String,
 }
-
 
 pub async fn get_lessons(client: &Client) -> Result<Lessons, String> {
     let unix_time = SystemTime::UNIX_EPOCH.elapsed().unwrap().as_millis();
@@ -73,7 +75,74 @@ pub async fn get_lessons(client: &Client) -> Result<Lessons, String> {
                                         url,
                                         name,
                                         teacher,
+                                        entry_latest: None,
+                                        entries: None,
                                     })
+                                }
+                            }
+
+                            // Get latest lesson entry
+                            let school_classes_selector = Selector::parse("tr.printable").unwrap();
+                            let school_classes = document.select(&school_classes_selector);
+                            for school_class in school_classes {
+                                let teacher_selector = Selector::parse(".teacher").unwrap();
+                                let teacher = school_class.select(&teacher_selector).next();
+
+                                if let Some(date) = school_class.select(&Selector::parse(".datum").unwrap()).next() {
+                                    fn collect_text(element_ref: Option<ElementRef>) -> Result<String, ()> {
+                                        match element_ref {
+                                            Some(element_ref) => {
+                                                match element_ref.text().collect::<String>().trim().to_string() {
+                                                    Ok(s) => Ok(s),
+                                                    Err(_) => Err(())
+                                                }
+                                            }
+                                            None => Err(())
+                                        }
+                                    }
+                                    let topic_title_selector = Selector::parse(".thema").unwrap();
+                                    let topic_title = collect_text(school_class.select(&topic_title_selector).next()).unwrap_or("".to_string());
+
+                                    let teacher_short_selector =  Selector::parse(".teacher .btn.btn-primary.dropdown-toggle.btn-xs").unwrap();
+                                    let teacher_short = collect_text(school_class.select(&teacher_short_selector).next()).unwrap_or("".to_string());
+
+                                    let teacher_name_selector = Selector::parse(".teacher ul>li>a>i.fa").unwrap();
+                                    let teacher_name = collect_text(school_class.select(&teacher_name_selector).next()).unwrap_or("".to_string());
+
+                                    let topic_date_selector = Selector::parse(".datum").unwrap();
+                                    let topic_date = collect_text(school_class.select(&topic_date_selector).next()).unwrap_or("".to_string());
+
+                                    let course_url_selector = Selector::parse("td>h3>a").unwrap();
+                                    let course_url = school_class.select(&course_url_selector).next().map(|x| x.value().attr("href").unwrap().to_string().trim().to_string()).unwrap_or("".to_string());
+
+                                    let file_count_selector = Selector::parse("file").unwrap();
+                                    let file_count: i32 = school_class.select(&file_count_selector).count() as i32;
+
+                                    let homework_selector = Selector::parse(".homework").unwrap();
+                                    let homework = school_class.select(&homework_selector).next().map(|_| {
+                                        let description_selector = Selector::parse(".realHomework").unwrap();
+                                        let description = school_class.select(&description_selector).next().unwrap().text().collect::<String>().trim().to_string();
+                                        let completed = school_class.select(&Selector::parse(".undone").unwrap()).next().is_none();
+                                        Homework { description, completed }
+                                    });
+
+                                    let entry_id = school_class.value().attr("data-entry").unwrap_or("").to_string();
+
+                                    for lesson in lessons.lessons.iter_mut() {
+                                        if lesson.url == course_url.to_owned() {
+                                            lesson.entry_latest = Option::from(LessonEntry{
+                                                id: entry_id.to_owned(),
+                                                date: topic_date.to_owned(),
+                                                title: topic_title.to_owned(),
+                                                details: None,
+                                                homework: homework.to_owned(),
+                                                attachments: None,
+                                                attachment_number: file_count,
+                                                uploads: None,
+                                            })
+                                        }
+                                    }
+
                                 }
                             }
                             Ok(lessons)
@@ -94,3 +163,4 @@ pub async fn get_lessons(client: &Client) -> Result<Lessons, String> {
         }
     }
 }
+
