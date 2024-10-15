9ffb41ead9ef8067102128b02525b0ac055ea984
Rajala1404 <115874847+Rajala1404@users.noreply.github.com>
2024-10-15 17:21:00 +0200

    Finished implementation of handshake and key exchange

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@ mod tests {
     use crate::base::account::Account;
     use crate::base::schools::{get_school_id, get_schools, School};
     use crate::modules::lessons::{get_lessons};
-    use crate::utils::crypt::handshake;
     use super::*;
 
     #[test]
@@ -86,7 +85,7 @@ mod tests {
             },
             type_a: None,
             data: None,
-            key_pair: utils::crypt::generate_key_pair(128).await.unwrap()
+            key_pair: utils::crypt::generate_key_pair(128, &client).await.unwrap()
         };
 
         if !account.create_session(&client).await.is_ok() {
@@ -104,8 +103,6 @@ mod tests {
             println!("{:?}", &lesson);
         }
         print!("\n");
-
-        handshake(&client, &account.key_pair.public_key_string).await.unwrap();
 
         println!("Private Key:\n{}", account.key_pair.private_key_string);
         println!("Public Key:\n{}", account.key_pair.public_key_string);
--- src/utils/crypt.rs
+++ src/utils/crypt.rs
@@ -6,6 +6,7 @@ use serde::Deserialize;
 use md5::{Digest, Md5};
 use aes::cipher::{BlockDecryptMut, KeyIvInit};
 use aes::cipher::block_padding::NoPadding;
+use evpkdf::evpkdf;
 use crate::utils::constants::URL;
 
 type Aes256CbcDec = cbc::Decryptor<aes::Aes256>;
@@ -18,10 +19,12 @@ pub struct KeyPair {
     pub private_key_string: String,
     /// Public key in PKCS#8 format
     pub public_key_string: String,
+    /// Public key that's encoded and encrypted
+    pub public_key_lanis: String,
 }
 
 /// Takes key_size (in bits) and returns an RSA KeyPair
-pub async fn generate_key_pair(key_size: usize) -> Result<KeyPair, String> {
+pub async fn generate_key_pair(key_size: usize, client: &Client) -> Result<KeyPair, String> {
     let mut rng = rand::thread_rng();
     match RsaPrivateKey::new(&mut rng, key_size) {
         Ok(private_key) => {
@@ -34,7 +37,19 @@ pub async fn generate_key_pair(key_size: usize) -> Result<KeyPair, String> {
                 let private_key_string = private_key_string.unwrap().to_string();
                 let public_key_string = public_key_string.unwrap();
 
-                Ok(KeyPair { private_key, public_key, private_key_string, public_key_string })
+                match handshake(client, &public_key_string).await {
+                    Ok(public_key_lanis) => {
+                        Ok(KeyPair{
+                            private_key,
+                            public_key,
+                            private_key_string,
+                            public_key_string,
+                            public_key_lanis,
+                        })
+                    }
+                    Err(e) => Err(format!("Handshake with lanis failed with error: '{}'", e)),
+                }
+
             } else {
                 Err("Failed to convert private key and/or public key to pkcs8 pem!".to_string())
             }
@@ -45,7 +60,7 @@ pub async fn generate_key_pair(key_size: usize) -> Result<KeyPair, String> {
     }
 }
 
-pub(crate) async fn handshake(client: &Client, public_own_key: &String) -> Result<(), String> {
+async fn handshake(client: &Client, public_own_key: &String) -> Result<String, String> {
     let mut rng = rand::thread_rng();
     let public_key = get_public_key(&client).await?;
 
@@ -61,7 +76,7 @@ pub(crate) async fn handshake(client: &Client, public_own_key: &String) -> Resul
             headers.insert("Sec-Fetch-Mode", "cors".parse().unwrap());
             headers.insert("Sec-Fetch-Site", "same-origin".parse().unwrap());
 
-            match client.post(URL::AJAX).headers(headers).query(&[("f", "rsaHandshake"), ("s", "1111")]).form(&[("key", encrypted_key)]).send().await {
+            match client.post(URL::AJAX).headers(headers).query(&[("f", "rsaHandshake"), ("s", "1111")]).form(&[("key", &encrypted_key)]).send().await {
                 Ok(response) => {
                     #[derive(Debug, Deserialize)]
                     struct ResponseData {
@@ -73,7 +88,13 @@ pub(crate) async fn handshake(client: &Client, public_own_key: &String) -> Resul
                             match base64::Engine::decode(&base64::engine::general_purpose::STANDARD, &data.challenge) {
                                 Ok(challenge) => {
                                     let result = decrypt_with_key(&challenge, public_own_key).await?;
-                                    Ok(())
+                                    let result_string = String::from_utf8_lossy(&result);
+                                    let result_string = result_string.trim();
+                                    if result_string == public_own_key.trim() {
+                                        Ok(encrypted_key)
+                                    } else {
+                                        Err(format!("Failed to perform challenge! Public Keys don't match!:\nOwn Public Key:\n{}\n\nResponse Public Key:\n{}", public_own_key, result_string))
+                                    }
                                 }
                                 Err(e) => {
                                     Err(format!("Failed to decode challenge with error: '{}'", e))
@@ -94,7 +115,7 @@ pub(crate) async fn handshake(client: &Client, public_own_key: &String) -> Resul
     }
 }
 
-pub(crate) async fn get_public_key(client: &Client) -> Result<RsaPublicKey, String> {
+async fn get_public_key(client: &Client) -> Result<RsaPublicKey, String> {
     let mut headers = HeaderMap::new();
     headers.insert("Accept", "*/*".parse().unwrap());
     headers.insert("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8".parse().unwrap());
@@ -126,52 +147,11 @@ pub(crate) async fn get_public_key(client: &Client) -> Result<RsaPublicKey, Stri
 }
 
 pub(crate) async fn decrypt_with_key(data: &Vec<u8>, public_key: &String) -> Result<Vec<u8>, String> {
-    println!("Encrypted Data: \t\t\t'{}'", String::from_utf8_lossy(&data));
-
-
     fn is_salted(encrypted_data: &Vec<u8>) -> bool {
         match std::str::from_utf8(&encrypted_data[0..8]) {
             Ok(s) => s == "Salted__",
             Err(_) => false,
         }
-    }
-
-    pub fn bytes_to_keys(salt: &[u8], key: &[u8]) -> Vec<u8> {
-        let mut concatenated_hashes = vec![];
-        let mut current_hash = [0; 16];
-        let mut enough_bytes_for_key = false;
-        let mut pre_hash = vec![0; 0];
-
-        loop {
-            if !current_hash.iter().all(|&x| x == 0) {
-                pre_hash.extend_from_slice(key);
-                pre_hash.extend_from_slice(salt);
-            } else {
-                pre_hash.extend_from_slice(key);
-                pre_hash.extend_from_slice(salt);
-            }
-
-            current_hash = [0; 16];
-            let mut md5 = Md5::new();
-            md5::Digest::update(&mut md5, &pre_hash);
-
-            let mut hash_bytes: Vec<u8> = vec![];
-            {
-                let mut md5_clone = md5.clone();
-                md5::Digest::update(&mut md5_clone, &[]);
-                let result = md5_clone.finalize();
-                hash_bytes.extend_from_slice(result.as_ref());
-            }
-
-            concatenated_hashes.extend_from_slice(&hash_bytes);
-
-            if concatenated_hashes.len() >= 48 {
-                enough_bytes_for_key = true;
-                break;
-            }
-        }
-
-        concatenated_hashes
     }
 
     if !is_salted(&data) {
@@ -180,25 +160,23 @@ pub(crate) async fn decrypt_with_key(data: &Vec<u8>, public_key: &String) -> Res
 
     let salt = &data[8..16];
 
-    let key = bytes_to_keys(&salt, &public_key.as_bytes());
+    const KEY_SIZE: usize = 256;
+    const IV_SIZE: usize = 128;
 
-    let mut key_nv = [0; 32];
-    let mut iv = [0; 16];
+    let mut output = [0; (KEY_SIZE + IV_SIZE) / 8];
 
-    for i in 0..32 {
-        key_nv[i] = key[i]
-    }
+    evpkdf::<Md5>(public_key.as_bytes(), salt, 1, &mut output);
 
-    for i in 0..16 {
-        iv[i] = key[i+32]
-    }
+    let (key, iv) = output.split_at(KEY_SIZE / 8);
 
-    let decryptor = Aes256CbcDec::new(&key_nv.into(), &iv.into());
+    let key: [u8; 32] = key.try_into().unwrap();
+    let iv: [u8; 16] = iv.try_into().unwrap();
 
-    let plaintext = decryptor.decrypt_padded_vec_mut::<NoPadding>(&data[16..]).unwrap();
 
-    println!("Decrypted Data: \t\t\t{}", String::from_utf8_lossy(&plaintext));
+    let decryptor = Aes256CbcDec::new(&key.into(), &iv.into());
 
-    Ok(vec![])
+    let result = decryptor.decrypt_padded_vec_mut::<NoPadding>(&data[16..]).unwrap();
+
+    Ok(result)
 
 }
